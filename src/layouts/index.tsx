cd7ae1f0452617a13e7c2b0ddbb634bafc0d0d93
next <next@nextdeMacBook-Air.local>
2022-10-20 10:47:46 +0800

    test

---
+++ src/layouts/index.tsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+
+export default function Layout() {
+    return (
+        <div className={styles.navs}>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/docs">Docs</Link>
+                </li>
+                <li>
+                    <a href="https://github.com/umijs/umi">Github</a>
+                </li>
+                <li>
+                    <a href='/chentong'>陈通</a>
+                </li>
+            </ul>
+            <Outlet />
+        </div>
+    );
+}
---
+++ src/pages/chentong.tsx
@@ -0,0 +1,5 @@
+export default function ChenTong() {
+    return <div>
+        陈通
+    </div>
+}
